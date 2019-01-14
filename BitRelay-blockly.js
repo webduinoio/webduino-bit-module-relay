dee87e10e86d248fdac6685cb01f245051006923
sheng <karokya@gmail.com>
2019-01-14 14:12:09 +0800

    Rewrite the js of blockly

--- BitRelay-blockly.js
+++ BitRelay-blockly.js
@@ -2,8 +2,8 @@
 
   'use strict';
 
-  window.getRGBLed = function (board, red, green, blue) {
-    return new webduino.module.RGBLed(board, board.getDigitalPin(red), board.getDigitalPin(green), board.getDigitalPin(blue));
+  window.getRelay = function (board, pin) {
+    return new webduino.module.Relay(board, board.getDigitalPin(pin));
   };
 
 }(window, window.webduino));
